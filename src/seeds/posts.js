8b183205d86a0470d0a301e0049683296fcba771
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2018-09-22 20:39:39 +0100

    adding projects seed

--- src/seeds/posts.js
+++ src/seeds/posts.js
@@ -1,9 +1,22 @@
- const posts = [
-    {title : "Creating An Html5 Canvas Custom Rendrer With React Reconciliation" , timestamp : "21-09-2018"},
-    {title : "Creating A Template Framework Generator" , timestamp : "21-09-2018"},
-    {title : "Creating A Terminal Based Personal Website" , timestamp : "21-09-2018"},
-    {title : "Creating A Custom Rendrer With React Reconciliation" , timestamp : "21-09-2018"},
-    {title : "Creating A WebMethods DocumentType Generator" , timestamp : "21-09-2018"},
-    {title : "Creating A Custom WebMethods Mapper" , timestamp : "21-09-2018"},
-  ];
-  export default posts;
+const posts = [
+  {
+    title: "Creating An Html5 Canvas Custom Rendrer With React Reconciliation",
+    timestamp: "21-09-2018"
+  }, {
+    title: "Creating A Template Framework Generator",
+    timestamp: "21-09-2018"
+  }, {
+    title: "Creating A Terminal Based Personal Website",
+    timestamp: "21-09-2018"
+  }, {
+    title: "Creating A Custom Rendrer With React Reconciliation",
+    timestamp: "21-09-2018"
+  }, {
+    title: "Creating A WebMethods DocumentType Generator",
+    timestamp: "21-09-2018"
+  }, {
+    title: "Creating A Custom WebMethods Mapper",
+    timestamp: "21-09-2018"
+  }
+];
+export default posts;
---
+++ src/seeds/projects.js
@@ -0,0 +1,109 @@
+const projects = [
+    {
+        id:0,
+        title: "Shortcuts.io",
+        subTitle: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+        tags: [
+            {
+                label: "js",
+                color: "yellow"
+            }, {
+                label: "react",
+                color: "light-blue"
+            }
+        ]
+    }, {
+        id:1,
+        title: "cuisine.ma",
+        subTitle: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+        tags: [
+            {
+                label: "js",
+                color: "yellow"
+            }, {
+                label: "react",
+                color: "light-blue"
+            }
+        ]
+    }, {
+        id:2,
+        title: "Reprise App",
+        subTitle: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+        tags: [
+            {
+                label: "js",
+                color: "yellow"
+            }, {
+                label: "react",
+                color: "light-blue"
+            }
+        ]
+    }, {
+        id:3,
+        title: "bounty.ma",
+        subTitle: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+        tags: [
+            {
+                label: "js",
+                color: "yellow"
+            }, {
+                label: "react",
+                color: "light-blue"
+            }
+        ]
+    }, {
+        id:4,
+        title: "Feed.ma",
+        subTitle: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+        tags: [
+            {
+                label: "js",
+                color: "yellow"
+            }, {
+                label: "react",
+                color: "light-blue"
+            }
+        ]
+    }, {
+        id:5,
+        title: "curated.io",
+        subTitle: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+        tags: [
+            {
+                label: "js",
+                color: "yellow"
+            }, {
+                label: "react",
+                color: "light-blue"
+            }
+        ]
+    }, {
+        id:6,
+        title: "ourStory",
+        subTitle: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+        tags: [
+            {
+                label: "js",
+                color: "yellow"
+            }, {
+                label: "react",
+                color: "light-blue"
+            }
+        ]
+    }, {
+        id:7,
+        title: "WebMethods School",
+        subTitle: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+        tags: [
+            {
+                label: "js",
+                color: "yellow"
+            }, {
+                label: "react",
+                color: "light-blue"
+            }
+        ]
+    }
+];
+
+export default projects;
