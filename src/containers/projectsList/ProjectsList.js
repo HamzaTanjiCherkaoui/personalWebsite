66d466d2adb6007b13c4393f136d5f8c55cea391
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2018-09-23 18:10:23 +0100

    react router gh-pages issue

--- src/containers/projectsList/ProjectsList.js
+++ src/containers/projectsList/ProjectsList.js
@@ -8,7 +8,7 @@ export default class ProjectsList extends Component {
       <React.Fragment>
         {projects.map(project => {
             return(
-                <Link to={`${process.env.PUBLIC_URL}/projectDetails/${project.id}`} key={project.id} >
+                <Link to={process.env.PUBLIC_URL+`/projectDetails/${project.id}`} key={project.id} >
                 <Card  {...project} effect="hover" />   
                 </Link>
             )
