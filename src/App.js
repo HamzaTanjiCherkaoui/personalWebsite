71f11cd77f27f7a9dda3428e2b5227223a668ea1
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2018-09-23 17:44:37 +0100

    react router gh-pages issue

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import React, {Component} from 'react';
 import './App.css';
 import Home from './containers/home/Home';
 import ProjectDetails from './containers/projectDetails/ProjectDetails';
-import {BrowserRouter as Router,  Route,  } from 'react-router-dom';
+import {BrowserRouter,  Route,  } from 'react-router-dom';
 class App extends Component {
   render() {
     
     return (
-      <Router>
+      <BrowserRouter  basename={process.env.PUBLIC_URL}>
       
         <div className="wrapper">
-        <Route exact path={process.env.PUBLIC_URL+"/"} component={Home}/>
-        <Route exact path={process.env.PUBLIC_URL+"/projectDetails/:projectId"} component={ProjectDetails}/>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/projectDetails/:topicId" component={ProjectDetails}/>
                   </div>
                   <div className="footer">
                   Copyrights - {(new Date()).getFullYear() }
                 </div>
       
-      </Router>
+      </BrowserRouter>
     );
   }
 }
